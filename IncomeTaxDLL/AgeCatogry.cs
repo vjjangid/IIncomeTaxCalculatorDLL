a377312dbe363172ac4d6e405cc3ab49a95bdbec
Vijay Jangid <vjjangid850@gmail.com>
2021-03-12 14:06:02 +0530

    User details class created

---
+++ IncomeTaxDLL/AgeCatogry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeTaxDLL
+{
+    public class AgeCatogry
+    {
+        public enum UserAgeCatogry
+        {
+            /// <summary>
+            /// Age below 60
+            /// </summary>
+            General,
+
+            /// <summary>
+            /// Age between 60 to 80 exclude 80
+            /// </summary>
+            Senior,
+
+            /// <summary>
+            /// Age above 80
+            /// </summary>
+            SuperSenior
+        };
+    }
+}
---
+++ IncomeTaxDLL/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeTaxDLL
+{
+
+    /// <summary>
+    /// User Details class
+    /// </summary>
+    public class User
+    {
+        private string _userName;
+        
+        /// <summary>
+        /// Username
+        /// </summary>
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                _userName = value;
+            }
+        }
+
+        /// <summary>
+        /// Date of birth of User
+        /// </summary>
+        public DateTime? DOB { get; set; }
+
+        /// <summary>
+        /// Whether user salaried or not
+        /// </summary>
+        public bool IsSalaried { get; set; }
+
+        /// <summary>
+        /// Whether user resides in metro or not
+        /// </summary>
+        public bool ResidesInMetro { get; set; }
+
+        public AgeCatogry.UserAgeCatogry AgeClass
+        {
+            get;
+            set;
+        }
+    }
+}
