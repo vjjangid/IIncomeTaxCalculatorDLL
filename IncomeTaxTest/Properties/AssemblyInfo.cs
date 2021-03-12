e22a7e45548cce847d4742d18d04ee4381c3d5d1
Vijay Jangid <vjjangid850@gmail.com>
2021-03-12 14:18:04 +0530

    Test Project for Income Tax DLL

---
+++ IncomeTaxTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IncomeTaxTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IncomeTaxTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("720063b3-90f9-4f39-a19e-443388482e93")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IncomeTaxTest/UserTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace IncomeTaxTest
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void UserNameTestValid()
+        {
+
+        }
+    }
+}
