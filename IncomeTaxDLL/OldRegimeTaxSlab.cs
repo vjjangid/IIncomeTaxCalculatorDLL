655939e37f48e559ba9bdc331cf37fc335d949a6
Vijay Jangid <vjjangid850@gmail.com>
2021-03-16 14:19:40 +0530

    Old Tax Regime implemented

---
+++ IncomeTaxDLL/OldRegimeTaxSlab.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeTax
+{
+    /// <summary>
+    /// Tax slabs under old tax regime
+    /// </summary>
+    public class OldRegimeTaxSlab
+    {
+        
+        /// <summary>
+        /// Only public function to calculate the tax
+        /// </summary>
+        /// <param name="ageCategory">Age category of person</param>
+        /// <param name="taxableAmount">Taxable amount</param>
+        /// <returns></returns>
+        public int calculateTax(AgeCategory ageCategory, double taxableAmount)
+        {
+            double totalTax = 0;
+
+            if (ageCategory == AgeCategory.General)
+                totalTax = CalcualteGeneralSlabTaxOldRegime(taxableAmount);
+            else if (ageCategory == AgeCategory.Senior)
+                totalTax = CalcualteSeniorSlabTaxOldRegime(taxableAmount);
+            else
+                totalTax = CalcualteSuperSeniorSlabTaxOldRegime(taxableAmount);
+
+            return (int)(totalTax);
+        }
+
+
+        /// <summary>
+        /// Caluclating tax for general age category which <=60
+        /// </summary>
+        /// <param name="taxableAmount">Total gross income</param>
+        /// <returns></returns>
+        private double CalcualteGeneralSlabTaxOldRegime(double taxableAmount)
+        {
+            double totalTax = 0;
+            //No tax upto 250000
+            if (taxableAmount <= 250000)
+                return totalTax;
+
+            //No tax upto 250000
+            taxableAmount -= 250000;
+            Console.WriteLine("total Gross Salary is :: " + taxableAmount);
+            Console.WriteLine("Total tax till now is :: " + totalTax);
+
+            //Five percent tax for next 250000;
+            if (taxableAmount - 250000 <= 0)
+            {
+                Console.WriteLine("5 percent if");
+                totalTax += taxableAmount * (0.05);
+                return totalTax;
+            }
+            else
+            {
+                Console.WriteLine("5 percent else");
+                totalTax = (250000 * 0.05);
+                taxableAmount = taxableAmount - 250000;
+            }
+
+            Console.WriteLine("total Gross Salary is :: " + taxableAmount);
+            Console.WriteLine("Total tax till now is :: " + totalTax);
+
+            //20 percent tax for next  5 lakhs
+            if (taxableAmount - 500000 <= 0)
+            {
+                Console.WriteLine("20 percent if");
+                totalTax += taxableAmount * (0.2);
+                Console.WriteLine("Total Tax :: " + totalTax);
+                return totalTax;
+            }
+            else
+            {
+                Console.WriteLine("20 percent else");
+                totalTax += 500000 * (0.2);
+                taxableAmount = taxableAmount - 500000;
+            }
+
+            Console.WriteLine("total Gross Salary is :: " + taxableAmount);
+            Console.WriteLine("Total tax till now is :: " + totalTax);
+
+            //30 percent tax for remaining amount
+            totalTax += taxableAmount * (0.3);
+
+            Console.WriteLine("total Gross Salary is :: " + taxableAmount);
+            Console.WriteLine("Total tax till now is :: " + totalTax);
+            return totalTax;
+
+        }
+
+        /// <summary>
+        /// Calculating tax for senior citizen category
+        /// </summary>
+        /// <param name="taxableAmount">the total gross income</param>
+        /// <returns></returns>
+        private double CalcualteSeniorSlabTaxOldRegime(double taxableAmount)
+        {
+            double totalTax = 0;
+            //No tax upto 300000
+            if (taxableAmount <= 300000)
+                return totalTax;
+
+            //No tax upto 300000
+            taxableAmount -= 300000;
+            Console.WriteLine("total Gross Salary is :: " + taxableAmount);
+            Console.WriteLine("Total tax till now is :: " + totalTax);
+
+            //Five percent tax for next 200000;
+            if (taxableAmount - 200000 <= 0)
+            {
+                Console.WriteLine("5 percent if");
+                totalTax += taxableAmount * (0.05);
+                return totalTax;
+            }
+            else
+            {
+                Console.WriteLine("5 percent else");
+                totalTax = (200000 * 0.05);
+                taxableAmount = taxableAmount - 200000;
+            }
+
+            Console.WriteLine("total Gross Salary is :: " + taxableAmount);
+            Console.WriteLine("Total tax till now is :: " + totalTax);
+
+            //20 percent tax for next  5 lakhs
+            if (taxableAmount - 500000 <= 0)
+            {
+                Console.WriteLine("20 percent if");
+                totalTax += taxableAmount * (0.2);
+                Console.WriteLine("Total Tax :: " + totalTax);
+                return totalTax;
+            }
+            else
+            {
+                Console.WriteLine("20 percent else");
+                totalTax += 500000 * (0.2);
+                taxableAmount = taxableAmount - 500000;
+            }
+
+            Console.WriteLine("total Gross Salary is :: " + taxableAmount);
+            Console.WriteLine("Total tax till now is :: " + totalTax);
+
+            //30 percent tax for remaining amount
+            totalTax += taxableAmount * (0.3);
+
+            Console.WriteLine("total Gross Salary is :: " + taxableAmount);
+            Console.WriteLine("Total tax till now is :: " + totalTax);
+            return totalTax;
+
+        }
+
+        /// <summary>
+        /// Calculate tax for super senior citizen
+        /// </summary>
+        /// <param name="taxableAmount"></param>
+        /// <returns></returns>
+        private double CalcualteSuperSeniorSlabTaxOldRegime(double taxableAmount)
+        {
+            double totalTax = 0;
+            //No tax upto 500000
+            if (taxableAmount <= 500000)
+                return totalTax;
+
+            //No tax upto 500000
+            taxableAmount -= 500000;
+            Console.WriteLine("total Gross Salary is :: " + taxableAmount);
+            Console.WriteLine("Total tax till now is :: " + totalTax);
+
+
+            //20 percent tax for next  5 lakhs
+            if (taxableAmount - 500000 <= 0)
+            {
+                Console.WriteLine("20 percent if");
+                totalTax += taxableAmount * (0.2);
+                Console.WriteLine("Total Tax :: " + totalTax);
+                return totalTax;
+            }
+            else
+            {
+                Console.WriteLine("20 percent else");
+                totalTax += 500000 * (0.2);
+                taxableAmount = taxableAmount - 500000;
+            }
+
+            Console.WriteLine("total Gross Salary is :: " + taxableAmount);
+            Console.WriteLine("Total tax till now is :: " + totalTax);
+
+            //30 percent tax for remaining amount
+            totalTax += taxableAmount * (0.3);
+
+            Console.WriteLine("total Gross Salary is :: " + taxableAmount);
+            Console.WriteLine("Total tax till now is :: " + totalTax);
+            return totalTax;
+
+        }
+    }
+}
---
+++ IncomeTaxTest/OldRegimeTaxSlabTest.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using IncomeTax;
+
+namespace IncomeTaxTest
+{
+    /// <summary>
+    /// Testing the slab rates of tax
+    /// </summary>
+    [TestClass]
+    public class OldRegimeTaxSlabTest
+    {
+        /// <summary>
+        /// Check the Gerneral category perosn having no taxable income
+        /// </summary>
+        [TestMethod]
+        public void GenrealSlabNoTaxCategory()
+        {
+            //--Arrange
+            OldRegimeTaxSlab old = new OldRegimeTaxSlab();
+            int expected = 0;
+
+            //--Act
+            int actual = old.calculateTax(AgeCategory.General, 250000);
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
+        /// <summary>
+        /// General category salary upto 5Lakh
+        /// </summary>
+        [TestMethod]
+        public void GenrealSlab5PercentTaxCategory()
+        {
+            //--Arrange
+            OldRegimeTaxSlab old = new OldRegimeTaxSlab();
+            int expected = 12500;
+
+            //--Act
+            int actual = old.calculateTax(AgeCategory.General, 500000);
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        /// <summary>
+        /// General category salary upto 10Lakh
+        /// </summary>
+        [TestMethod]
+        public void GenrealSlab20PercentTaxCategory()
+        {
+            //--Arrange
+            OldRegimeTaxSlab old = new OldRegimeTaxSlab();
+            int expected = 12500 + 100000;
+
+            //--Act
+            int actual = old.calculateTax(AgeCategory.General, 1000000);
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        /// <summary>
+        /// General category salary after 10Lakh
+        /// </summary>
+        [TestMethod]
+        public void GenrealSlab30PercentTaxCategory()
+        {
+            //--Arrange
+            OldRegimeTaxSlab old = new OldRegimeTaxSlab();
+            int expected = 12500 + 100000 + 60000;
+
+            //--Act
+            int actual = old.calculateTax(AgeCategory.General, 1200000);
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        /// <summary>
+        /// Check the Senior category perosn having no taxable income
+        /// </summary>
+        [TestMethod]
+        public void SeniorSlabNoTaxCategory()
+        {
+            //--Arrange
+            OldRegimeTaxSlab old = new OldRegimeTaxSlab();
+            int expected = 0;
+
+            //--Act
+            int actual = old.calculateTax(AgeCategory.Senior, 300000);
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        /// <summary>
+        /// Check the Senior category perosn having income upto 5Lakh
+        /// </summary>
+        [TestMethod]
+        public void SeniorSlab5PercentTaxCategory()
+        {
+            //--Arrange
+            OldRegimeTaxSlab old = new OldRegimeTaxSlab();
+            int expected = 10000;
+
+            //--Act
+            int actual = old.calculateTax(AgeCategory.Senior, 500000);
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        /// <summary>
+        /// Check the Senior category perosn having income upto 10Lakh
+        /// </summary>
+        [TestMethod]
+        public void SeniorSlab20PercentTaxCategory()
+        {
+            //--Arrange
+            OldRegimeTaxSlab old = new OldRegimeTaxSlab();
+            int expected = 10000 + 100000;
+
+            //--Act
+            int actual = old.calculateTax(AgeCategory.Senior, 1000000);
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        /// <summary>
+        /// Check the Senior category perosn having income more than 10Lakh
+        /// </summary>
+        [TestMethod]
+        public void SeniorSlab30PercentTaxCategory()
+        {
+            //--Arrange
+            OldRegimeTaxSlab old = new OldRegimeTaxSlab();
+            int expected = 10000 + 100000 + 60000;
+
+            //--Act
+            int actual = old.calculateTax(AgeCategory.Senior, 1200000);
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        /// <summary>
+        /// Check the Super Senior category perosn having no taxable income
+        /// </summary>
+        [TestMethod]
+        public void SuperSeniorSlabNoTaxCategory()
+        {
+            //--Arrange
+            OldRegimeTaxSlab old = new OldRegimeTaxSlab();
+            int expected = 0;
+
+            //--Act
+            int actual = old.calculateTax(AgeCategory.SuperSenior, 500000);
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        /// <summary>
+        /// Check the Super Senior category perosn having income upto 10L
+        /// </summary>
+        [TestMethod]
+        public void SuperSeniorSlab20PercentTaxCategory()
+        {
+            //--Arrange
+            OldRegimeTaxSlab old = new OldRegimeTaxSlab();
+            int expected = 100000;
+
+            //--Act
+            int actual = old.calculateTax(AgeCategory.SuperSenior, 1000000);
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        /// <summary>
+        /// Check the Super Senior category perosn having income more then 10L
+        /// </summary>
+        [TestMethod]
+        public void SuperSeniorSlab30PercentTaxCategory()
+        {
+            //--Arrange
+            OldRegimeTaxSlab old = new OldRegimeTaxSlab();
+            int expected = 100000 + 60000;
+
+            //--Act
+            int actual = old.calculateTax(AgeCategory.SuperSenior, 1200000);
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
