3fe2d628733843ce796a25477f21d36185483ff8
Vijay Jangid <vjjangid850@gmail.com>
2021-03-16 15:14:25 +0530

    Surcharge interface implementation

---
+++ IncomeTaxDLL/ISurchargeTax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeTax
+{
+    interface ISurchargeTax
+    {
+        //if income is > 50L
+        bool Surcharge10Percent { get; }
+
+        //if income is > 1 crore
+        bool Surcharge15Percent { get; }
+
+        //if income is > 2 crore
+        bool Surcharge25Percent { get; }
+
+        //if income is > 5 crore
+        bool Surcharge37Percent { get; }
+
+        int GetSurcharge();
+
+
+    }
+}
